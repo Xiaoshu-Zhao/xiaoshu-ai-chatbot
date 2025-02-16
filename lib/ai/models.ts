977f1e309b7394e4c75b3f1a623c13a2f673b7f1
Xiaoshu-Zhao <xz2987@columbia.edu>
2025-02-16 19:44:24 +0800

    将后端模型升级

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -11,11 +11,8 @@ export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
 export const myProvider = customProvider({
   languageModels: {
     'chat-model-small': openai('gpt-4o-mini'),
-    'chat-model-large': openai('gpt-4o-mini'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
-    }),
+    'chat-model-large': openai('gpt-4o-2024-11-20'),
+    'chat-model-reasoning': openai('o3-mini-2025-01-31'),
     'title-model': openai('gpt-4-turbo'),
     'block-model': openai('gpt-4o-mini'),
   },
@@ -34,12 +31,17 @@ interface ChatModel {
 export const chatModels: Array<ChatModel> = [
   {
     id: 'chat-model-small',
-    name: 'Small model',
-    description: 'Small model for fast, lightweight tasks',
+    name: 'gpt-4o-mini',
+    description: '小模型用于快速、轻量级的任务',
   },
   {
     id: 'chat-model-large',
-    name: 'Large model',
-    description: 'Large model for complex, multi-step tasks',
-  }
+    name: 'gpt-4o',
+    description: 'openai 的门面模型，适用于更加复杂的场景',
+  },
+  {
+    id: 'chat-model-reasoning',
+    name: 'o3',
+    description: '当前最强的推理模型，用于解决复杂的，多步骤的逻辑推理问题',
+  },
 ];
